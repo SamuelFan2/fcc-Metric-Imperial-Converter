c5385f8384f75ef24eee0cfd7363c0b0e1e143c2
SamuelFan2 fan <smallwarxxx1@gmail.com>
2024-03-01 08:12:21 +0000

    Feat: convert function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,41 +1,52 @@
 function ConvertHandler() {
   
+  this.unitArr = {
+    gal: ['L', 3.78541, 'gallons'],
+    L: ['gal', 1/3.78541, 'liters' ],
+    mi: ['km', 1.60934, 'miles' ],
+    km: ['mi', 1/1.60934, 'kilometers' ],
+    lbs: ['kg', 0.453592, 'pounds'],
+    kg: ['lbs', 1/0.453592, 'kilograms']
+  }
+  
   this.getNum = function(input) {
     let result;
-    
-    return result;
+    let regex = /(\d*(?:\.\d*)?)(\/)?(\d*(?:\.\d*)?)(.*)/;
+    result = input.replace(regex, (_match, a, b, c, d) => {
+      console.log(a,b,c,d);
+      if (b === '/') {
+        return Number(a)/Number(c)
+      }
+      return a;
+    });
+    return Number(result);
   };
   
   this.getUnit = function(input) {
     let result;
-    
+    let regex = /(\d*(?:\.\d*)?)(\/)?(\d*(?:\.\d*)?)(.*)/;
+    result = input.replace(regex, (_match, a, b, c, d) => d);
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+    result = this.unitArr[initUnit][0];
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
+    let result = this.unitArr[unit][2];
     return result;
   };
   
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    let result = this.unitArr[initUnit][1] * initNum;
+    return Number(result.toFixed(5));
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
+    let result = `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,15 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum,initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let string = convertHandler.getString(initNum,initUnit,returnNum,returnUnit);
+    console.log(input,initNum,initUnit,returnNum,returnUnit);
+
+    res.json({ initNum: initNum, initUnit: initUnit, returnNum: returnNum, returnUnit: returnUnit, string: string })
+  })
 };
